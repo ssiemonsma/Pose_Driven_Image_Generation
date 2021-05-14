76d7eb433905446de059b7f426dbb056a258e5aa
Stephen Siemonsma <42848857+ssiemonsma@users.noreply.github.com>
2021-05-14 01:05:31 -0500

    Add files via upload

--- NADS-Net_with_PVT/util/util.py
+++ NADS-Net_with_PVT/util/util.py
@@ -6,13 +6,11 @@ Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses
 import re
 import importlib
 import torch
-from argparse import Namespace
 import numpy as np
 from PIL import Image
 import os
 import argparse
 import dill as pickle
-import util.coco
 
 
 def save_obj(obj, name):
@@ -217,47 +215,6 @@ def load_network(net, label, epoch, opt):
 def uint82bin(n, count=8):
     """returns the binary of integer n, count refers to amount of bits"""
     return ''.join([str((n >> y) & 1) for y in range(count - 1, -1, -1)])
-
-
-def labelcolormap(N):
-    if N == 35:  # cityscape
-        cmap = np.array([(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (111, 74, 0), (81, 0, 81),
-                         (128, 64, 128), (244, 35, 232), (250, 170, 160), (230, 150, 140), (70, 70, 70), (102, 102, 156), (190, 153, 153),
-                         (180, 165, 180), (150, 100, 100), (150, 120, 90), (153, 153, 153), (153, 153, 153), (250, 170, 30), (220, 220, 0),
-                         (107, 142, 35), (152, 251, 152), (70, 130, 180), (220, 20, 60), (255, 0, 0), (0, 0, 142), (0, 0, 70),
-                         (0, 60, 100), (0, 0, 90), (0, 0, 110), (0, 80, 100), (0, 0, 230), (119, 11, 32), (0, 0, 142)],
-                        dtype=np.uint8)
-    else:
-        cmap = np.zeros((N, 3), dtype=np.uint8)
-        for i in range(N):
-            r, g, b = 0, 0, 0
-            id = i + 1  # let's give 0 a color
-            for j in range(7):
-                str_id = uint82bin(id)
-                r = r ^ (np.uint8(str_id[-1]) << (7 - j))
-                g = g ^ (np.uint8(str_id[-2]) << (7 - j))
-                b = b ^ (np.uint8(str_id[-3]) << (7 - j))
-                id = id >> 3
-            cmap[i, 0] = r
-            cmap[i, 1] = g
-            cmap[i, 2] = b
-
-        if N == 182:  # COCO
-            important_colors = {
-                'sea': (54, 62, 167),
-                'sky-other': (95, 219, 255),
-                'tree': (140, 104, 47),
-                'clouds': (170, 170, 170),
-                'grass': (29, 195, 49)
-            }
-            for i in range(N):
-                name = util.coco.id2label(i)
-                if name in important_colors:
-                    color = important_colors[name]
-                    cmap[i] = np.array(list(color))
-
-    return cmap
-
 
 class Colorize(object):
     def __init__(self, n=35):

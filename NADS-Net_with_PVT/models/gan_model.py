f2cac7b5ba8ee7f0d06d21c5a7ff4ad1f64098c1
Stephen Siemonsma <42848857+ssiemonsma@users.noreply.github.com>
2021-05-14 01:16:12 -0500

    Update gan_model.py

--- NADS-Net_with_PVT/models/gan_model.py
+++ NADS-Net_with_PVT/models/gan_model.py
@@ -12,22 +12,7 @@ import torch.nn.functional as F
 def MSE_criterion(input, target):
     return nn.MSELoss()(input, target)
 
-class DiceBCELoss(nn.Module):
-    def __init__(self, weight=None, size_average=True):
-        super(DiceBCELoss, self).__init__()
-
-    def forward(self, inputs, targets, smooth=1):
-        inputs = inputs.view(-1)
-        targets = targets.detach().view(-1)
-
-        intersection = (inputs * targets).sum()
-        dice_loss = 1 - (2. * intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
-        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
-        Dice_BCE = BCE + dice_loss
-
-        return Dice_BCE
-
-DiceBCE_criterion = DiceBCELoss()
+DiceBCE_criterion = networks.DiceBCELoss()
 
 class GAN_Model(torch.nn.Module):
     @staticmethod
